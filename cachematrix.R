c330e2e41e5a60dd61758831ae70eb5ad97ff961
cnewcom7 <63749636+cnewcom7@users.noreply.github.com>
2020-04-30 16:06:43 -0400

    Add files via upload

--- cachematrix.R
+++ cachematrix.R
@@ -2,14 +2,57 @@
 ## functions do
 
 ## Write a short comment describing this function
+## cnewcom7
+## makeCacheMatrix takes a matrix argument and cache's its calc'd inverse
 
 makeCacheMatrix <- function(x = matrix()) {
-
+  
+  invMat <- NULL    ## set the variable to store the inverse matrix as empty/NULL to start
+  
+  
+  # Resets matrix without needed to call entire makeCacheMatrix
+  # Use "variable-name"$set(matrix(a:b, x-dim, y-dim)) format to change matrix and clear cache storage
+  set <- function(y= matrix()) {
+    x <<- y 
+    invMat <<- NULL  
+  }
+  
+  get <- function() x # Returns the original matrix
+  
+  setInvMat <- function(solve) invMat <<- solve
+  
+  getInvMat <- function() invMat # Returns what's stored in invMat (either NULL or inverse'd matrix)
+  
+  # Set identifiers to easily call each function above
+  list(set = set, 
+       get = get,
+       setInvMat = setInvMat,
+       getInvMat = getInvMat)  
 }
 
 
 ## Write a short comment describing this function
+## cacheSolve calculates the inverse of the matrix entered in the above function 
 
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+  
+  # Imports the value stored in invMat
+  # Checks if invMat is empty or not
+  # If empty then moves to next section to perform inverse calc; if not empty returns the cache'd data
+  invMat <- x$getInvMat() 
+  
+  if(!is.null(invMat)) {
+    
+    message("Getting cached data...")
+    invMat
+    
+  }
+  
+  # Import the original matrix and use 'solve' function to calc inverse, then cache output in invMat variable
+  matrix <- x$get()  
+  invMat <- solve(matrix, ...)  
+  x$setInvMat(invMat) 
+  
+  ## Returns the inverse of the matrix 'x'
+  invMat
 }
